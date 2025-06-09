3bd7d05d1d110e6e955183122e568c5a999e9c07
K-SAISIDDHANTH <saisiddhanth12@gmail.com>
2025-06-09 18:46:52 +0530

    installed popups lib

--- src/Screen/Components/Card.jsx
+++ src/Screen/Components/Card.jsx
@@ -1,67 +1,51 @@
 import '../Cards.css'
 import Popup from 'reactjs-popup'
-function Cards({data }) {
+function Cards({ data }) {
 
-  
   return (
-    
-
     <Popup trigger={<button className="cardcomp">
       <div className="maincard">
-
-      
-          <div className="img" >
-
-            <img src="https://th.bing.com/th/id/OIP.7BSdgl7f0znwH8JGQYisGgHaE4?rs=1&pid=ImgDetMain" alt="" />
+        <div className="img" >
+          <img src="https://th.bing.com/th/id/OIP.7BSdgl7f0znwH8JGQYisGgHaE4?rs=1&pid=ImgDetMain" alt="" />
+        </div>
+        <div>
+          <h4>
+            Name:{data.attributes.name}
+          </h4>
+        </div>
+        <div>
+          <h4>description:</h4>  <p className="desc" >{data.attributes.description}</p>
+        </div>
+        <div style={{ display: "flex" }}>
+          <div style={{ paddingRight: "20px" }}>
+            <h4>
+              MinAge:{data.attributes.life.min}
+            </h4>
           </div>
           <div>
             <h4>
-              Name:{data.attributes.name}
+              MaxAge:{data.attributes.life.max}
             </h4>
           </div>
-
-          <div>
-
-            <h4>description:</h4>  <p className="desc" >{data.attributes.description}</p>
-
-          </div>
-
-
-          <div style={{ display: "flex" }}>
-            <div style={{ paddingRight: "20px" }}>
-              <h4>
-                MinAge:{data.attributes.life.min}
-              </h4>
-            </div>
-            <div>
-              <h4>
-                MaxAge:{data.attributes.life.max}
-              </h4>
-            </div>
-          </div>
-          </div>
+        </div>
+      </div>
     </button>}
-    modal
-    position="center center">
-          
+      modal
+      position="center center">
       <div className="maincard Popup">
         <div className="nav-bar">
           <button className="btns" >x</button></div>
-            <div className="image">
-             <img src="https://th.bing.com/th/id/OIP.7BSdgl7f0znwH8JGQYisGgHaE4?rs=1&pid=ImgDetMain" alt="" />
-             </div>
-            <h3>Name:{data.attributes.name}</h3>
-            <h4>Description:</h4><p>{data.attributes.description}</p>
-            <div className="age">
-                <div className="a"><p>MinAge:{data.attributes.life.min}</p></div>
-                <div className="a"><p>MaxAge:{data.attributes.life.max}</p></div>
-            </div>
-            
-    </div>
-          
+        <div className="image">
+          <img src="https://th.bing.com/th/id/OIP.7BSdgl7f0znwH8JGQYisGgHaE4?rs=1&pid=ImgDetMain" alt="" />
+        </div>
+        <h3>Name:{data.attributes.name}</h3>
+        <h4>Description:</h4><p>{data.attributes.description}</p>
+        <div className="age">
+          <div className="a"><p>MinAge:{data.attributes.life.min}</p></div>
+          <div className="a"><p>MaxAge:{data.attributes.life.max}</p></div>
+        </div>
+      </div>
     </Popup>
-
-    
   )
 }
 
--- src/Screen/Components/Maincomponent.jsx
+++ src/Screen/Components/Maincomponent.jsx
@@ -3,9 +3,6 @@ import SearchBar from "./Searchbar";
 import Cards from "./Card";
 import '../Maincomponent.css';
 
-
-
-
 function Maincomponent({ dogData }) {
   const [value,setValue]=useState("")
   return (
@@ -13,17 +10,14 @@ function Maincomponent({ dogData }) {
       <div className="mainBox">
         {/* search bar */}
         <SearchBar setValue={setValue} value={value}/>
-
         <div className="cards">
           {/* calling card components */}
           {
             dogData.filter((inpt)=>{
               return value.toLowerCase() ===" "  ?    setSearchData(true):inpt.attributes.name.toLowerCase().includes(value)}
             ).map((data) => {
-             
-            //Calling the card components while passing props
+             //Calling the card components while passing props
               return <Cards data={data}/>
-              
             })
           }   
         </div>
